78b5da75d39dbaf8b50e10323d727a510d8e782c
Susan Subedi <ssubedi1@go.olemiss.edu>
2020-05-15 18:09:48 -0500

    ListView Builder widget for the appbar and quick options done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:Messenger/widgets/catergory_selector.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +9,29 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        elevation: 0.0,
+        title: Text("Chats",
+            style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold)),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Column(
+        children: <Widget>[CategorySelector()],
+      ),
+    );
   }
 }
